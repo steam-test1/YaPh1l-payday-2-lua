07fb9e6b26f40d204dffa9f5950fd53c3a6ef7bd
Philipp <philipprippmann@aol.de>
2017-05-12 16:58:28 +0200

    v1.70.234 (U145.1)

--- lib/managers/menumanager.lua
+++ lib/managers/menumanager.lua
@@ -8108,7 +8108,7 @@ function MenuChooseWeaponRewardInitiator:modify_node(original_node, data)
 	local function chk_parent_func(weapon)
 		return weapon_tweak[weapon.weapon_id] and not weapon_tweak[weapon.weapon_id].parent_weapon_id
 	end
-	local category, loot_table, data
+	local loot_table, data
 	for i, category_data in ipairs({primaries, secondaries}) do
 		for _, weapon_data in ipairs(category_data) do
 			if chk_dlc_func(weapon_data) and chk_parent_func(weapon_data) then
@@ -8116,10 +8116,11 @@ function MenuChooseWeaponRewardInitiator:modify_node(original_node, data)
 				if #loot_table > 0 then
 					data = deep_clone(weapon_data)
 					data.loot_table = loot_table
-					category = weapon_tweak[weapon_data.weapon_id].category
-					category = tweak_data.gui.buy_weapon_category_groups[category] or category
-					items[category] = items[category] or {}
-					table.insert(items[category], data)
+					for _, category in ipairs(weapon_tweak[weapon_data.weapon_id].categories) do
+						category = tweak_data.gui.buy_weapon_category_groups[category] or category
+						items[category] = items[category] or {}
+						table.insert(items[category], data)
+					end
 				end
 			end
 		end
